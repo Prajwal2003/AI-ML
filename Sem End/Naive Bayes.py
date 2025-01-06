934370cbeb80b7f62095dcc7e8eeb3035858630c
Prajwal G Hoolageri <prajwalgh13@gmail.com>
2025-01-06 07:54:45 +0530

    Update Naive Bayes.py

--- Sem End/Naive Bayes.py
+++ Sem End/Naive Bayes.py
@@ -3,17 +3,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 
-# Load dataset
 iris = load_iris()
 X, y = iris.data, iris.target
 
-# Split dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Train Naive Bayes
 model = GaussianNB()
 model.fit(X_train, y_train)
 
-# Test the model
 y_pred = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
